3496ff8cd2dbdcdd151ccd829490c17293affa8c
EC2 Default User <ec2-user@ip-172-31-36-164.ap-northeast-1.compute.internal>
2024-06-28 12:45:09 +0000

    [add]mypageとログインの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,20 +2,20 @@ class UsersController < ApplicationController
   # before_action :set_user, only: [:show]
   before_action :authenticate_user!, only: [:mypage]
   # # new必要？
-  def new
-    @user=User.new
-  end
-  # # create必要？
-  def create
-    @user = User.new(user_params[:id])
+  # def new
+  #   @user=User.new
+  # end
+  # # # create必要？
+  # def create
+  #   @user = User.new(user_params[:id])
     
-    if @user.save
-      redirect_to user_path(current_user)
-    else
-      # @users = User.all 
-      render :new
-    end
-  end
+  #   if @user.save
+  #     redirect_to user_path(current_user)
+  #   else
+  #     # @users = User.all 
+  #     render :new
+  #   end
+  # end
   
   
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  devise_for :users
   root to: "homes#top"
+  get 'users/mypage' => 'users#mypage'
   resources :users, only: [:show,:edit,:update,:destroy]
-  get '/mypage' => 'users#mypage'
+ 
 end
