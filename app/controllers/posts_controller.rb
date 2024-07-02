78cef125bc7a544018ad7597c290a167383dd8ec
EC2 Default User <ec2-user@ip-172-31-36-164.ap-northeast-1.compute.internal>
2024-07-02 12:16:20 +0000

    [add]投稿更新作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,7 +30,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
-    redirect_to post_path[:id]
+    redirect_to post_path(@post.id)
   end
   
   def destroy
