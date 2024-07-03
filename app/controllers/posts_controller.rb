6ca1da23b32e83848fabc98db6595ba1d4703b9b
EC2 Default User <ec2-user@ip-172-31-36-164.ap-northeast-1.compute.internal>
2024-07-03 15:47:43 +0000

    [add]投稿削除機能作成中

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
   def destroy
     post = Post.find(params[:id])
     post.destroy
-    redirect_to 
+    redirect_to users_mypage_path(@post)
   end
   
     # 投稿データのストロングパラメータ
