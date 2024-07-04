7fb711b1034b5a3cda97be8c6c69fc71741d2b8c
EC2 Default User <ec2-user@ip-172-31-36-164.ap-northeast-1.compute.internal>
2024-07-04 16:43:20 +0000

    [add]マイページ編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,15 @@ class User < ApplicationRecord
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
   end
+  
+  # プロフィール写真
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 
   private
 
