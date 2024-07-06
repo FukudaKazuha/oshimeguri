4e19e01688c82777260bd1b23314cfc0852382e3
EC2 Default User <ec2-user@ip-172-31-36-164.ap-northeast-1.compute.internal>
2024-07-06 10:56:27 +0000

    [add]他ユーザーのアクセス制限の編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,11 +27,11 @@ class PostsController < ApplicationController
   end
   
   def edit
+    @user = User.find(params[:id])
     user = User.find(params[:id])
     unless user.id == current_user.id
      redirect_to edit_post_path
     end
-     @post = Post.find(params[:id])
   end
   
   def update
