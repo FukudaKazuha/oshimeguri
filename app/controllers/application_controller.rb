a0785b8f98d23f7fa2c7c6f2087f48611d582cd7
EC2 Default User <ec2-user@ip-172-31-36-164.ap-northeast-1.compute.internal>
2024-08-09 12:43:19 +0000

    非ログイン時のアクセスの制限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   # ログインしているかどうか判断
-  before_action :authenticate_user
+  before_action :require_login, except: [:top]
+  
+  def set_current_user
+    @current_user=User.find_by(id :session[:user_id])
+  end
 
   
   def after_sign_in_path_for(resource)
@@ -19,17 +23,19 @@ class ApplicationController < ActionController::Base
     if resource_class == User
     # サインアップ、メールとパスワード追加
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username,:email,:password])
-    # サインイン、
+    # サインイン、ユーザーネームとパスワード
       devise_parameter_sanitizer.permit(:sign_in,keys:[:username,:password])
       # devise_parameter_sanitizer.permit(:account_update,keys:[:name,:email])
     end
   end
   
-  def authenticate_user
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
-  end
+ 
+  private
   
+  def require_login
+   if !current_user && (controller_name != 'sessions' && controller_name != 'registrations')
+    redirect_to root_path, alert: "ログインが必要です"
+   end
+  end
 
 end
