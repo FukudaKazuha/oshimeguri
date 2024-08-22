e4d9989b76360e39d8a57772cc8bdecb8d58e7c5
fukuda <fk2000.js7@gmail.com>
2024-08-22 12:16:09 +0000

    検索機能編集

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,12 +4,12 @@ class SearchesController < ApplicationController
   def search
     @range = params[:range]
 
-    if @range == "User"
+    if @range == "ユーザー"
       @users = User.looks(params[:search], params[:word])
+      # byebug
     else
       @posts = Post.looks(params[:search], params[:word])
     end
-   redirect_to search_path
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -41,13 +41,13 @@ class User < ApplicationRecord
   # 検索方法分岐
   def self.looks(search, word)
     if search == "perfect_match"
-      @user = User.where("name LIKE?", "#{word}")
+      @user = User.where("username LIKE?", "#{word}")
     elsif search == "forward_match"
-      @user = User.where("name LIKE?","#{word}%")
+      @user = User.where("username LIKE?","#{word}%")
     elsif search == "backward_match"
-      @user = User.where("name LIKE?","%#{word}")
+      @user = User.where("username LIKE?","%#{word}")
     elsif search == "partial_match"
-      @user = User.where("name LIKE?","%#{word}%")
+      @user = User.where("username LIKE?","%#{word}%")
     else
       @user = User.all
     end
