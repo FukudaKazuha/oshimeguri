edcb172a9b7fca1f3ea8493c3d854f023aebe741
fukuda <fk2000.js7@gmail.com>
2024-10-04 09:15:36 +0000

    いいね機能の実装追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,9 +4,7 @@ class LikesController < ApplicationController
   def create
    post = Post.find(params[:post_id])
    like = current_user.likes.new(post_id: post.id)
-   # @post = Post.find(post_params[:id])
-   # @user = User.find(params[:id])
-   @post_like.save
+   like.save
    redirect_to post_path(post)
   end
   
@@ -14,18 +12,19 @@ class LikesController < ApplicationController
   def index
    @posts=current_user.like_posts
   end
+  
 
   def destroy
-   like = Like.find_by(user_id: @user.id, post_id: @post.id)
+   post = Post.find(params[:post_id])
+   like = current_user.likes.find_by(post_id: post.id)
    like.destroy
-   redirect_to post_path(@post)
+   redirect_to post_path(post)
   end
   
   private
   
   def set_user_and_post
    @user = current_user
-  # @post = Post.find(params[:id])
   end
   
   def post_params
