6474d065f11a6251626031f7603a64c140e8e916
fukuda <fk2000.js7@gmail.com>
2024-10-09 07:18:52 +0000

    テスト２

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,9 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   # userとのアソシエーション
   has_many :post_comments, dependent: :destroy
-  
+  # テスト
+  has_many :likes, dependent: :destroy
+  has_many :like_posts, through: :likes, source: :post
 
 # ゲストログイン
   def self.guest
