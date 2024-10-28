228997b283ae6b0da38c9d4812122d0f7778716f
fukuda <fk2000.js7@gmail.com>
2024-10-28 12:19:49 +0000

    評価後の修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,7 +44,7 @@ class PostsController < ApplicationController
   def destroy
    post = Post.find(params[:id])
    post.destroy
-   redirect_to user_mypage_path
+   redirect_to users_mypage_path
   end
   
   
